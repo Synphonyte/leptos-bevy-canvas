df6f7dfd1d2607bf8359e13696b5c763464db245
Maccesch <maccesch@synphonyte.com>
2025-02-04 14:33:18 -0500

    fixed sync query example

--- examples/synced_bevy_query/src/events.rs
+++ examples/synced_bevy_query/src/events.rs
@@ -1,11 +1,4 @@
-use bevy::prelude::*;
-
-#[derive(Event, Debug)]
-pub struct TextEvent {
-    pub text: String,
-}
-
-#[derive(Event, Debug, Copy, Clone)]
-pub struct ClickEvent {
-    pub char_index: usize,
-}
+// #[derive(Event, Debug, Copy, Clone)]
+// pub struct ClickEvent {
+//     pub char_index: usize,
+// }
--- examples/synced_bevy_query/src/leptos_app.rs
+++ examples/synced_bevy_query/src/leptos_app.rs
@@ -1,35 +1,12 @@
 use crate::bevy_app::init_bevy_app;
-use crate::events::{ClickEvent, TextEvent};
 use crate::{RENDER_HEIGHT, RENDER_WIDTH};
 use bevy::prelude::{Name, Transform, With};
 use leptos::prelude::*;
 use leptos_bevy_canvas::prelude::*;
-use leptos_use::use_debounce_fn;
-
-#[derive(Copy, Clone)]
-pub enum EventDirection {
-    None,
-    LeptosToBevy,
-    BevyToLeptos,
-}
 
 #[component]
 pub fn App() -> impl IntoView {
     let (selected, selected_query_duplex) = single_query_signal::<(Name,), With<Transform>>();
-
-    let input = move || {
-        selected.read().as_ref().map(|(name,)| {
-            view! {
-                <input
-                    type="text"
-                    value=name.to_string()
-                    on:input=move |ev| {
-                        selected.write().as_mut().map(|(name,)| name.set(event_target_value(&ev)));
-                    }
-                />
-            }
-        })
-    };
 
     view! {
         <div class="flex w-full mx-auto max-w-[1400px] p-5 items-center">
@@ -52,7 +29,13 @@ pub fn App() -> impl IntoView {
             <Frame class="border-blue-500 bg-blue-500/5 max-w-[200px]">
                 <h2 class="text-xl font-bold text-blue-500 relative top-[-10px]">Leptos</h2>
 
-                {input}
+                <input
+                    type="text"
+                    prop:value=move || selected.read().as_ref().map(|(name,)| name.to_string()).unwrap_or_default()
+                    on:input=move |ev| {
+                        selected.write().as_mut().map(|(name,)| name.set(event_target_value(&ev)));
+                    }
+                />
             </Frame>
         </div>
     }
