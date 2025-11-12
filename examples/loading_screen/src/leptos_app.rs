373249218cbbe8e074a28fb0eaa5466b4645ac34
Joel Beicher <joel@synphonyte.com>
2025-11-12 15:36:44 +0100

    changed loading indicator

--- examples/loading_screen/src/leptos_app.rs
+++ examples/loading_screen/src/leptos_app.rs
@@ -94,62 +94,55 @@ pub fn LeptosOverlay(state: RwSignalSynced<AppState>) -> impl IntoView {
                 <div class=loading_class>
                     <div class="relative w-full text-blue-500 rounded-md">
                         <svg
-                            version="1.1"
-                            id="L7"
+                            viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                            x="0px"
-                            y="0px"
-                            viewBox="0 0 100 100"
-                            enable-background="new 0 0 100 100"
-                            xml:space="preserve"
-                            class="transform-gpu stroke-white scale-[0.3]"
+                            class="scale-[0.1] fill-blue-400"
                         >
-                            <path
-                                fill="currentColor"
-                                d="M31.6,3.5C5.9,13.6-6.6,42.7,3.5,68.4c10.1,25.7,39.2,38.3,64.9,28.1l-3.1-7.9c-21.3,8.4-45.4-2-53.8-23.3
-                                c-8.4-21.3,2-45.4,23.3-53.8L31.6,3.5z"
-                            >
+                            <defs>
+                                <filter id="spinner-gF01">
+                                    <feGaussianBlur
+                                        in="SourceGraphic"
+                                        stdDeviation="1"
+                                        result="y"
+                                    />
+                                    <feColorMatrix
+                                        in="y"
+                                        mode="matrix"
+                                        values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7"
+                                        result="z"
+                                    />
+                                    <feBlend in="SourceGraphic" in2="z" />
+                                </filter>
+                            </defs>
+                            <g filter="url(#spinner-gF01)">
+                                <circle cx="5" cy="12" r="4">
+                                    <animate
+                                        attributeName="cx"
+                                        calcMode="spline"
+                                        dur="2s"
+                                        values="5;8;5"
+                                        keySplines=".36,.62,.43,.99;.79,0,.58,.57"
+                                        repeatCount="indefinite"
+                                    />
+                                </circle>
+                                <circle cx="19" cy="12" r="4">
+                                    <animate
+                                        attributeName="cx"
+                                        calcMode="spline"
+                                        dur="2s"
+                                        values="19;16;19"
+                                        keySplines=".36,.62,.43,.99;.79,0,.58,.57"
+                                        repeatCount="indefinite"
+                                    />
+                                </circle>
                                 <animateTransform
                                     attributeName="transform"
-                                    attributeType="XML"
                                     type="rotate"
-                                    dur="2s"
-                                    from="0 50 50"
-                                    to="360 50 50"
+                                    dur="0.75s"
+                                    values="0 12 12;360 12 12"
                                     repeatCount="indefinite"
                                 />
-                            </path>
-                            <path
-                                fill="currentColor"
-                                d="M42.3,39.6c5.7-4.3,13.9-3.1,18.1,2.7c4.3,5.7,3.1,13.9-2.7,18.1l4.1,5.5c8.8-6.5,10.6-19,4.1-27.7
-                                c-6.5-8.8-19-10.6-27.7-4.1L42.3,39.6z"
-                            >
-                                <animateTransform
-                                    attributeName="transform"
-                                    attributeType="XML"
-                                    type="rotate"
-                                    dur="1s"
-                                    from="0 50 50"
-                                    to="-360 50 50"
-                                    repeatCount="indefinite"
-                                />
-                            </path>
-                            <path
-                                fill="currentColor"
-                                d="M82,35.7C74.1,18,53.4,10.1,35.7,18S10.1,46.6,18,64.3l7.6-3.4c-6-13.5,0-29.3,13.5-35.3s29.3,0,35.3,13.5
-                                L82,35.7z"
-                            >
-                                <animateTransform
-                                    attributeName="transform"
-                                    attributeType="XML"
-                                    type="rotate"
-                                    dur="2s"
-                                    from="0 50 50"
-                                    to="360 50 50"
-                                    repeatCount="indefinite"
-                                />
-                            </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
